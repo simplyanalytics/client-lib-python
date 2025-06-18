3304677dec19fb8c60d55b365f8a2b3174783293
Sascha <smartens@simplyanalytics.com>
2025-06-18 10:12:20 -0700

    reformat with black

--- simplyanalytics/simplyanalytics.py
+++ simplyanalytics/simplyanalytics.py
@@ -19,21 +19,18 @@ class SimplyAnalyticsClient:
             params["k"] = self.key
 
         response = requests.post(
-            self.url,
-            params=params,
-            json=data,
-            cookies=self._cookies
+            self.url, params=params, json=data, cookies=self._cookies
         )
 
         json = response.json()
 
-        if 'exception' in json:
-            raise Exception(json['message'])
+        if "exception" in json:
+            raise Exception(json["message"])
 
         return response.json()
 
     def get_attributes(self, data: dict) -> list:
-        return self.query("get", "attributes", data)['hits']
+        return self.query("get", "attributes", data)["hits"]
 
     def get_locations(self, data: dict) -> list:
         return self.query("get", "data/locations2", data)
@@ -44,7 +41,7 @@ class SimplyAnalyticsClient:
         year: Optional[int] = None,
         country: Optional[str] = None,
         census_release: Optional[int] = None,
-        limit: int = 100
+        limit: int = 100,
     ) -> list:
         where: list[str | int | list] = [
             "and",
@@ -59,9 +56,11 @@ class SimplyAnalyticsClient:
         if census_release:
             where.append(["=", "censusRelease", census_release])
 
-        return self.get_attributes({
-            "where": where,
-            "fields": ["attribute", "name", "type"],
-            "slice": [0, limit],
-            "sort": [["asc", "grouped_order"]]
-        })
+        return self.get_attributes(
+            {
+                "where": where,
+                "fields": ["attribute", "name", "type"],
+                "slice": [0, limit],
+                "sort": [["asc", "grouped_order"]],
+            }
+        )
