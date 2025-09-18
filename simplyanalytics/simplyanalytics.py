d5e8455aa0bf8d9b2a2bf704473330d2e7675aee
Sascha <smartens@simplyanalytics.com>
2025-09-18 10:41:23 -0700

    add aggregate_data method

--- simplyanalytics/simplyanalytics.py
+++ simplyanalytics/simplyanalytics.py
@@ -199,3 +199,13 @@ class SimplyAnalyticsClient:
             query["slice"] = slice
 
         return self.get_locations(query)
+
+    def aggregate_data(self, attributes: list[str], where: list) -> list:
+        query = {
+            "select": attributes,
+            "aggregates": [{
+                "locationSeries": where
+            }]
+        }
+
+        return self.get_locations(query)
